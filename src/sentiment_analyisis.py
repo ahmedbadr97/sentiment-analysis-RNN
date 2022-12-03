2d68a4f8d7de3748da11be8f1cfc5ef48ce8f337
Ahmed Badr <ahmed.k.badr.97@gmail.com>
2022-12-04 01:23:52 +0200

    add predict using cuda

--- src/sentiment_analyisis.py
+++ src/sentiment_analyisis.py
@@ -4,11 +4,13 @@ import torch
 from . import data_preprocessing
 
 model = None
+current_device = 'cpu'
 
 
 def load_model(weights_path: str, word2dict_path, device='cpu', parameters: dict = None):
     global model
-
+    global current_device
+    current_device = device
     word2dict = utils.load_json(word2dict_path)
     if parameters is None:
         model = SentimentAnalysisModel(word2dict, hidden_nodes=256, n_layers=2, embedding_dim=300)
@@ -22,6 +24,7 @@ def load_model(weights_path: str, word2dict_path, device='cpu', parameters: dict
 
 def predict(review: str):
     global model
+    global current_device
     if model is None:
         raise Exception("please load the model first")
     review = data_preprocessing.remove_punctuations(review)
@@ -32,11 +35,9 @@ def predict(review: str):
     for word in review_words:
         if word in model.word2int:
             int_review.append(model.word2int[word])
+    review_tensor = torch.tensor([int_review], dtype=torch.long, device=current_device)
 
     model.eval()
-    out,hidden=model(torch.tensor([int_review],dtype=torch.long))
+    out, hidden = model(review_tensor)
 
     return out.item()
-
-
-
