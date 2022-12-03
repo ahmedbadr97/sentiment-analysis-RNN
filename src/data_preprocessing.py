710444384e9da5f4697b18d9ed6567e66b25008d
Ahmed Badr <ahmed.k.badr.97@gmail.com>
2022-12-03 15:52:28 +0200

    add to remove noise remove words that doesn't appear less than n reviews

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -58,13 +58,14 @@ def get_noise_words(reviews_txt, labels_txt, threshold):
     return noisy_words
 
 
-def remove_noise(txt: str, filtering_ratio, prob_threshold=0.5, min_freq=2):
+def remove_noise(txt: str, filtering_ratio, prob_threshold=0.5, min_freq=2, min_rev_freq=None):
     # we will split the txt to lines  to keep the \n
 
     # till -1 to drop last empty line after the last \n
 
     word_counts = Counter(txt.split())
     total_count = sum(word_counts.values())
+    word_rev_count = Counter()
 
     word_weight = {word: count / total_count for word, count in word_counts.items()}
     p_drop = {word: 1 - np.sqrt(1.0 / (word_weight[word] * filtering_ratio)) for word in word_counts}
@@ -73,11 +74,22 @@ def remove_noise(txt: str, filtering_ratio, prob_threshold=0.5, min_freq=2):
     lines = txt.split('\n')
 
     new_txt = []
+
+    if min_rev_freq is not None:
+        for line in lines:
+            word_split_set = set(line.split())
+            for word in word_split_set:
+                word_rev_count[word] += 1
+
     for line in lines:
         word_split_txt = line.split()
 
         for word in word_split_txt:
-            if p_drop[word] <= prob_threshold and word_counts[word] >= min_freq:
+            not_noise = p_drop[word] <= prob_threshold and word_counts[word] >= min_freq
+            if min_rev_freq is not None:
+                not_noise &= word_rev_count[word] >= min_rev_freq
+
+            if not_noise:
                 new_txt.append(word)
                 new_txt.append(" ")
             else:
